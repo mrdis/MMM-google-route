0233a4f7b25056e00005180eb084ef1083f2de92
mrdis <31987174+mrdis@users.noreply.github.com>
2018-05-19 11:10:49 +0200

    Add directions request and display

--- MMM-google-route.js
+++ MMM-google-route.js
@@ -3,7 +3,8 @@ Module.register("MMM-google-route", {
     defaults: {
         key: 'apy_key',
         height: '300px',
-        width: '300px'
+        width: '300px',
+        directionsRequest:{}
     },
 
     getDom: function () {
@@ -18,9 +19,39 @@ Module.register("MMM-google-route", {
         script.src = "https://maps.googleapis.com/maps/api/js?key=" + this.config.key;
         document.body.appendChild(script);
 
+        var self = this;
         script.onload = function () {            
             var map = new google.maps.Map(document.getElementById("map"), {
             });
+            var directionsService = new google.maps.DirectionsService;
+            var directionsDisplay0 = new google.maps.DirectionsRenderer({
+                suppressMarkers : true,
+                polylineOptions:{
+                    strokeColor:'#fff',
+                    strokeWeight:7
+                }
+            });
+            var directionsDisplay1 = new google.maps.DirectionsRenderer({
+                suppressMarkers : true,
+                polylineOptions:{
+                    strokeColor:'#aaa',
+                    strokeWeight:5
+                }
+            });
+            directionsDisplay0.setMap(map);
+            directionsDisplay1.setMap(map);
+            directionsService.route(
+                self.config.directionsRequest
+                , 
+                function(response, status) {
+                    if (status === 'OK') {
+                        directionsDisplay0.setDirections(response);
+                        directionsDisplay1.setDirections(response);
+                        directionsDisplay1.setRouteIndex(1);
+                    } else {
+                        console.error('Directions request failed due to ' + status);
+                    }
+                });
         };
 
         return wrapper;
